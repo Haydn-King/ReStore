a799f3a884c28777223803f2b74b3e4cf0e1cb37
Haydn King <ahmillikin@gmail.com>
2021-11-19 16:48:45 -0800

    CRUD Inventory Complete

---
+++ API/Data/Migrations/20211119210110_PublicIdAdded.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class PublicIdAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PublicId",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "4e64a4af-4c7e-4fb6-9ccd-04aff1a35064");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "d7ee8362-0756-4d5b-ba94-73cf90617a4f");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "Products");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "826712fb-1780-42a8-b2a4-d0d044ec4814");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "e4369f4c-4186-4046-88d3-d5a628f015ef");
+        }
+    }
+}
